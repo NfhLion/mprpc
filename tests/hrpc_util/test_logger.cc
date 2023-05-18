241a9a323b876296ee1f2c6e1175eb24fd5d318d
ningfenghai <nfh15735388157@163.com>
2023-05-18 09:31:23 +0800

    [ADD] add log case

--- tests/hrpc_util/test_logger.cc
+++ tests/hrpc_util/test_logger.cc
@@ -4,13 +4,29 @@
 
 using namespace hrpc::util;
 
+// 获取core时落盘日志的方法：https://www.yuque.com/linuxer/xngi03/pfq9m1?
+void testCoreDump() {
+    for (int i = 0; i < 1000000; i++) {
+        LOG_INFO("test info log print, i = %d", i);
+        int* p = nullptr;
+        if (i == 500000) {
+            *p = 0x1234;
+        }
+    }
+}
+
+void testPrintLog() {
+    for (int i = 0; i < 100; i++) {
+        LOG_INFO("test info log print, i = %d", i);
+    }
+}
+
 int main(int argc, char* argv[]) {
     
     Logger::GetInstance().Init("./config/log.conf");
 
-    for (int i = 0; i < 100000; i++) {
-        LOG_INFO("test info log print, i = %d", i);
-    }
+    testPrintLog();
+    // testCoreDump();
 
     return 0;
 }
